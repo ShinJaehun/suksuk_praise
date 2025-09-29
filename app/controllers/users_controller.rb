87fe2ec34985b8f761116100de963f1b79e275c0
ShinJaehun <gunsmoke.shin@gmail.com>
2025-09-29 22:07:01 +0900

    feat(users): 옵션 C 라우트 가드레일 추가 및 교실 컨텍스트 뷰 개선

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,16 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
     before_action :set_user, only: [:show]
 
-    #def index
-        #@students = User.student
-    #end
-
     def show
-        @classroom = load_and_authorize_classroom!(params[:classroom_id]) if params[:classroom_id].present?
+        # @classroom = load_and_authorize_classroom!(params[:classroom_id]) if params[:classroom_id].present?
+
+        # 0) 교실 컨텍스트 로드(있는 경우만)
+        if params[:classroom_id].present?
+            # 현재 로그인 유저의 교실 접근 권한 확인
+            @classroom = load_and_authorize_classroom!(params[:classroom_id])
+            # 상세 대상(@user)이 그 교실 멤버인지 검증(아니면 404)
+            ensure_membership_for_user_in_classroom!(@user, @classroom)
+        end
 
         # 1) 사용자 페이지 접근 권한
         authorize @user, :show?
@@ -16,7 +20,6 @@ class UsersController < ApplicationController
         @compliments = policy_scope(Compliment)
                         .where(receiver_id: @user.id)
         @compliments = @compliments.where(classroom_id: @classroom.id) if @classroom
-
         @compliments = @compliments
                         .includes(:giver, :classroom)
                         .order(given_at: :desc)
@@ -33,5 +36,12 @@ class UsersController < ApplicationController
         classroom = Classroom.find(cid) # 못 찾으면 ActiveRecord::RecordNotFound
         authorize classroom, :show?
         classroom
+    end
+
+    # ---- 가드레일 핵심: 상세 대상(@user)이 해당 교실의 '실제 멤버'인지 확인 ----
+    def ensure_membership_for_user_in_classroom!(user, classroom)
+        # 연관이 있다면 classroom.users.exists?(user.id) 로도 가능
+        is_member = classroom.classroom_memberships.exists?(user_id: user.id)
+        raise ActiveRecord::RecordNotFound unless is_member
     end
 end
